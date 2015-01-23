9d87056699c2c08818a8146f62a6e5f4084cb9bc
vangheem <vangheem@gmail.com>
2015-01-23 14:36:57 -0600

    rename readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.elasticsearch',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
