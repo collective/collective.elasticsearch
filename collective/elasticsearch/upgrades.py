b0564a6c02cf5ee95c8b19d24b1a3fd6d4ee04d4
Gagaro <gagaro42@gmail.com>
2017-03-14 09:41:56 +0100

    fix: add upgrade steps for `IElasticSettings` update

---
+++ collective/elasticsearch/upgrades.py
@@ -0,0 +1,10 @@
+from Products.CMFCore.utils import getToolByName
+
+
+def update_registry(context):
+    portal_setup = getToolByName(context, 'portal_setup')
+    portal_setup.runImportStepFromProfile(
+        'profile-collective.elasticsearch:default',
+        'plone.app.registry',
+        run_dependencies=False,
+    )
