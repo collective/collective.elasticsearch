efc6929fe3306600f73a8c4107ed9f0c6ab2b1f4
Joel Kleier <joel.kleier@wildcardcorp.com>
2021-04-28 10:40:05 -0500

    Back to development: 4.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.elasticsearch',
-    version='4.0.0',
+    version='4.0.1.dev0',
     description="elasticsearch integration with plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
