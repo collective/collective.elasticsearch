c5fe81afec78c738737b6af64123c7033bfc4817
Érico Andrei <ericof@gmail.com>
2022-10-11 14:53:06 +0200

    In Plone 5.2 RelativeSession does not support the test param

--- src/collective/elasticsearch/tests/test_services.py
+++ src/collective/elasticsearch/tests/test_services.py
@@ -11,7 +11,7 @@ class TestService(BaseAPITest):
         self.portal_url = self.portal.absolute_url()
         self.request = self.portal.REQUEST
 
-        self.api_session = RelativeSession(self.portal_url, test=self)
+        self.api_session = RelativeSession(self.portal_url)
         self.api_session.headers.update({"Accept": "application/json"})
         self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
 
