05829b955a06f5d4c89345acd4d56a3ad02da1be
Mathias Leimgruber <m.leimgruber@webcloud7.ch>
2026-01-25 21:00:16 -0500

    Persist sessions in fetch per thread.

--- src/collective/elasticsearch/redis/fetch.py
+++ src/collective/elasticsearch/redis/fetch.py
@@ -1,20 +1,35 @@
+from collective.elasticsearch.local import get_local
+
 import io
 import os
 import requests
 
 
-session = requests.Session()
-session.headers.update({"Accept": "application/json"})
-session.auth = (
-    str(os.environ.get("PLONE_USERNAME", None)),
-    str(os.environ.get("PLONE_PASSWORD", None)),
-)
+def _create_session():
+    session = requests.Session()
+    session.headers.update({"Accept": "application/json"})
+    session.auth = (
+        str(os.environ.get("PLONE_USERNAME", None)),
+        str(os.environ.get("PLONE_PASSWORD", None)),
+    )
+    return session
 
-session_data = requests.Session()
-session_data.auth = (
-    str(os.environ.get("PLONE_USERNAME", None)),
-    str(os.environ.get("PLONE_PASSWORD", None)),
-)
+
+def _create_session_data():
+    session = requests.Session()
+    session.auth = (
+        str(os.environ.get("PLONE_USERNAME", None)),
+        str(os.environ.get("PLONE_PASSWORD", None)),
+    )
+    return session
+
+
+def get_session():
+    return get_local("fetch_session", _create_session)
+
+
+def get_session_data():
+    return get_local("fetch_session_data", _create_session_data)
 
 
 def fetch_data(plone_url, uuid, attributes):
@@ -22,7 +37,7 @@ def fetch_data(plone_url, uuid, attributes):
         plone_url = os.environ.get("PLONE_BACKEND", None)
     url = plone_url + "/@elasticsearch_extractdata"
     payload = {"uuid": uuid, "attributes:list": attributes}
-    response = session.get(url, params=payload, verify=False, timeout=60)
+    response = get_session().get(url, params=payload, verify=False, timeout=60)
     if response.status_code == 200:
         content = response.json()
         if "@id" in content and "data" in content:
@@ -37,5 +52,5 @@ def fetch_blob_data(plone_url, fieldname, data):
     download_url = "/".join(
         [plone_url, data[fieldname]["path"], "@@download", fieldname]
     )
-    file_ = session_data.get(download_url)
+    file_ = get_session_data().get(download_url)
     return io.BytesIO(file_.content)
